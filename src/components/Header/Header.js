f476884bf7b438aae7d0d4b10db337346cf8c51e
TetianaTS <devse.ten@uwp.digital>
2024-11-07 22:50:49 +0100

    add animations

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,33 @@
-import { NavLink } from "react-router-dom";
-import '../Header/Header.css'
-export const Header=()=>{
-        return(
-        <nav className= "Header">
-                <ul className = "Nav">
-                <li ><NavLink className="HomeLink" to="/">Home</NavLink></li>
-                <li ><NavLink className="HomeLink" to = "/catalog">Catalog</NavLink></li>
-                <li className="HomeLink"><NavLink className="HomeLink" to = "/favorites">Favorites</NavLink></li>
-             </ul> 
-             </nav>
-        )
-}
+import { NavLink } from 'react-router-dom';
+import logo from '../../icons/logo.png';
+import '../Header/Header.css';
+export const Header = () => {
+  return (
+    <nav className="Header">
+      <div>
+        <img className="logoImg" src={logo} alt="logo" />
+      </div>
+      <ul className="Nav">
+        <li>
+          <NavLink className="HomeLink" activeClassName="active" to="/">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className="HomeLink" activeClassName="active" to="/catalog">
+            Catalog
+          </NavLink>
+        </li>
+        <li className="HomeLink">
+          <NavLink
+            className="HomeLink"
+            activeClassName="active"
+            to="/favorites"
+          >
+            Favorites
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
